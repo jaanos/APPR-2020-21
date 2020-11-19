c990891a22fab487fc458007433d1386fe341ff0
Ana Marija Okorn <ana-marija.okorn@student.fmf.uni-lj.si>
2020-11-19 08:23:45 +0100

    dodane knjižnjice

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,11 +1,21 @@
 library(knitr)
 library(rvest)
 library(gsubfn)
-library(tidyr)
-library(tmap)
+library(reshape2)
 library(shiny)
-
-options(gsubfn.engine="R")
+library(tidyr)
+library(dygraphs)
+library(xts)
+library(tidyverse)
+library(lubridate)
+library(tmap)
+library("readxl")
+# library("openxlsx")
+source("https://raw.githubusercontent.com/jaanos/APPR-2019-20/master/lib/uvozi.zemljevid.r")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+
+options(gsubfn.engine="R")
+
+
