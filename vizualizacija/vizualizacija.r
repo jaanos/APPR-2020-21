d2a3021559417b4dd8b14d3520e2317ab92ee852
Ana Marija Okorn <ana-marija.okorn@student.fmf.uni-lj.si>
2020-11-19 16:40:38 +0100

    zakomentirani nepotrebni podatki

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -30,20 +30,22 @@ zemljevid1 <- tm_shape(merge(zemljevid, povprecje.diplomantov.po.regijah, by.x="
 graf1 <- ggplot(povprecje.dijakov.po.regijah, aes(regija, povprecje, group = 1)) + geom_col() + coord_flip() +
   labs(x = "Regija", y = "Povprecje", 
        title = "Povprečno število dijakov po regijah v zadnjih desetih letih")
-graf2 <- ggplot(povprecje.diplomantov.po.regijah, aes(regija, povprecje, group = 1)) + geom_col() + coord_flip() +
-  labs(x = "Regija", y = "Povprecje", 
-       title = "Povprečno število diplomantov po regijah v zadnjih desetih letih")
 
-m <- tabela2nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10) %>%filter(spol == "moski") %>% select(-"spol")
-z <- tabela2nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10) %>%filter(spol == "zenski")%>% select(-"spol")
-graf3 <- ggplot(m, aes(izobrazevanje, povprecje, group = 1)) + geom_col() + coord_flip() + labs(x = "Vrsta izobraževanja", y = "Število moških", 
-                                                                                                title = "Povprečno število moških v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih")
-graf4 <- ggplot(z, aes(izobrazevanje, povprecje, group = 1)) + geom_col() + coord_flip()+ labs(x = "Vrsta izobraževanja", y = "Število žensk", 
-                                                                                               title = "Povprečno število žensk v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih")
+#graf2 <- ggplot(povprecje.diplomantov.po.regijah, aes(regija, povprecje, group = 1)) + geom_col() + coord_flip() +
+#  labs(x = "Regija", y = "Povprecje", 
+#       title = "Povprečno število diplomantov po regijah v zadnjih desetih letih")
 
+#m <- tabela2nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10) %>%filter(spol == "moski") %>% select(-"spol")
+#z <- tabela2nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10) %>%filter(spol == "zenski")%>% select(-"spol")
+#graf3 <- ggplot(m, aes(izobrazevanje, povprecje, group = 1)) + geom_col() + coord_flip() + labs(x = "Vrsta izobraževanja", y = "Število moških", 
+#                                                                                                title = "Povprečno število moških v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih")
+#graf4 <- ggplot(z, aes(izobrazevanje, povprecje, group = 1)) + geom_col() + coord_flip()+ labs(x = "Vrsta izobraževanja", y = "Število žensk", 
+#                                                                                               title = "Povprečno število žensk v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih")
+
+#v graf5 združimo graf3 in graf4
 podatki5 <- tabela2nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10)
 
 #ggplot(podatki5, aes(x=izobrazevanje, y=povprecje, group = 1)) + geom_bar()  + coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "test")
 
-graf5 <- ggplot(podatki5, aes(x=izobrazevanje, y=povprecje, fill = spol)) + geom_col()  + 
-  coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "test")
+graf5 <- ggplot(podatki5, aes(x=izobrazevanje, y=povprecje, fill = spol)) + geom_col(position = 'dodge')  + 
+  coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "test") 
