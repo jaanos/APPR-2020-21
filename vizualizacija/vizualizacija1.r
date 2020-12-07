9d77bb1aee97890a6847b633bbfa657a7fde3d92
Urban Rupnik <urban.rupnik28@gmail.com>
2020-12-07 18:50:32 +0100

    Naredil histogram za vrste dohodka

--- vizualizacija/vizualizacija1.r
+++ vizualizacija/vizualizacija1.r
@@ -2,10 +2,14 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 
-vrste_dohodka_2019 <- vrste_dohodka %>%
-  filter(Leto == 2019) %>%
-  select(Vrsta.dohodka, Dohodek)
+vrste_dohodka_08_19 <- vrste_dohodka %>%
+  filter(Leto == 2008 | Leto == 2019) %>%
+  arrange(Vrsta.dohodka, desc(Leto), Dohodek) %>%
+  select(Vrsta.dohodka, Leto, Dohodek)
 
-graf_vrste_dohodka_2019 <- ggplot(vrste_dohodka_2019, aes(x=Vrsta.dohodka, y=Dohodek)) + 
-  geom_histogram(stat = "identity")
-print(graf_vrste_dohodka_2019)
+graf_vrste_dohodka_08_19 <- ggplot(vrste_dohodka_08_19, aes(x=Vrsta.dohodka, 
+                                                            y=Dohodek, fill=factor(Leto))) + 
+  geom_histogram(stat = "identity", binwidth=1, position="identity", color="black") +
+  labs(title="Dohodek glede na vrsto", x="Vrsta dohodka", y = "Dohodek", fill="Leto") +
+  scale_x_discrete(guide=guide_axis(n.dodge=2))
+print(graf_vrste_dohodka_08_19)
