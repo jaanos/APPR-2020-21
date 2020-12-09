e076cadaa79302b7bbd23d288f09f45db837d767
zigagartner <ziga.gartner@gmail.com>
2020-12-09 13:20:22 +0100

    Dodal tidy data za kapacitete, goste

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -8,6 +8,8 @@ library(stringr)
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
 letnice <- c(2010: 2019)
+tipi <- c("Zdraviliske obcine","Gorske obcine","Obmorske obcine","Ljubljana",
+        "Mestne obcine","Ostale obcine")
 
 # Prenocitve po vrstah obcin
 
@@ -15,13 +17,23 @@ prenocitve.vrste.obcin <- function(){
   uvoz <- read_csv2("podatki/Prenocitve_vrste_obcin.csv", locale=locale(encoding="Windows-1250"),
                     col_names=c("Tip", letnice)) 
   uvoz <- uvoz[-c(1,2),]
-  
-  return(uvoz)
+  uvoz <- uvoz[,-c(1)] 
+  uvoz <- t(uvoz)
+  Stevilo <- c(uvoz[,1], uvoz[,2], uvoz[,3], uvoz[,4], uvoz[,5], uvoz[,6])
+  Leta <- rep(2010:2019, 6)
+  Tipi <- rep(tipi, 10)
+  prenocitve <- data.frame(Tipi, Leta, Stevilo)
+  prenocitve[1:10, 1] <- tipi[1]
+  prenocitve[11:20, 1] <- tipi[2]
+  prenocitve[21:30, 1] <- tipi[3]
+  prenocitve[31:40, 1] <- tipi[4]
+  prenocitve[41:50, 1] <- tipi[5]
+  prenocitve[51:60, 1] <- tipi[6]
+  return(prenocitve)
 }
 
 prenocitve <- prenocitve.vrste.obcin()
 
-tip1 <- prenocitve[,1]
 
 
 
@@ -85,9 +97,19 @@ kapacitete.vrste.obcin <- function(){
   uvoz <- read_csv2("podatki/kapacitete_vrste_obcin.csv", locale=locale(encoding="Windows-1250"),
                     col_names=c("Tip", letnice))
   uvoz <- uvoz[-c(1,2),]
-  uvoz[, 1] <- tip1 
-  
-  return(uvoz)
+  uvoz <- uvoz[,-c(1)] 
+  uvoz <- t(uvoz)
+  Stevilo <- c(uvoz[,1], uvoz[,2], uvoz[,3], uvoz[,4], uvoz[,5], uvoz[,6])
+  Leta <- rep(2010:2019, 6)
+  Tipi <- rep(tipi, 10)
+  kapacitete <- data.frame(Tipi, Leta, Stevilo)
+  kapacitete[1:10, 1] <- tipi[1]
+  kapacitete[11:20, 1] <- tipi[2]
+  kapacitete[21:30, 1] <- tipi[3]
+  kapacitete[31:40, 1] <- tipi[4]
+  kapacitete[41:50, 1] <- tipi[5]
+  kapacitete[51:60, 1] <- tipi[6]
+  return(kapacitete)
 }
 
 
