e040f8ea95ba9ecc0cbcf2a8ab6187014e72912e
Urban Rupnik <urban.rupnik28@gmail.com>
2020-12-27 17:22:37 +0100

    Dodal uvoze knjižnic za uvoz in vizualizacijo

--- lib/libraries.r
+++ lib/libraries.r
@@ -9,3 +9,9 @@ options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+
+# Uvoz podatkov
+source("uvoz/uvoz1.r", encoding = "UTF-8")
+
+# Vizualizacije
+source("vizualizacija/vizualizacija1.r", encoding = "UTF-8")
