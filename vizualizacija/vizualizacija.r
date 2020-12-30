23178da30ea41b2400a20cbe7c21f09dcec8c495
nezakrzan <nezza.krzzan@gmail.com>
2020-12-30 08:32:52 +0100

    Analiza kriza 2008, primerjava z letom 2020

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -713,13 +713,53 @@ placa <- c(max(zasebnisektor_osmanj_zenske$placa),max(zasebnisektor_sr_zenske$pl
                min(zasebnisektor_osmanj_zenske$placa),min(zasebnisektor_sr_zenske$placa), min(zasebnisektor_vs_zenske$placa))
 placa_zasebnisektor_zenske <- data.frame(izobrazba, vrsta_place, placa)
 
+  #Graf primerjave minimalne in maksimalne place glede na izobrazbo
 ggplot(placa_javnisektor_moski, aes(x=izobrazba, y=placa)) +
   geom_point(color="dark blue", size=2) +
-  geom_point(placa_zasebnisektor_moski, aes(x=izobrazba, y=placa), color="dark green", size=2) +
-  geom_point(placa_javnisektor_zenske, aes(x=izobrazba, y=placa), color="red", size=2) +
-  geom_point(placa_zasebnisektor_zenske, aes(x=izobrazba, y=placa), color="pink", size=2) +
+  geom_point(placa_zasebnisektor_moski, mapping=aes(x=izobrazba, y=placa), color="dark green", size=2) +
+  geom_point(placa_javnisektor_zenske, mapping=aes(x=izobrazba, y=placa), color="red", size=2) +
+  geom_point(placa_zasebnisektor_zenske, mapping=aes(x=izobrazba, y=placa), color="pink", size=2) +
   labs(title="Primerjava minimalne in maksimalne plače v zasebnem in javnem sektorju") +
   ylab("Višina plače(€)") +
   xlab("Izobrazba")
   
+#Krizi(2008,2020)
+meseci <- c("12", "11","10","09","08","07","06","05","04","03","02","01",
+            "12", "11","10","09","08","07","06","05","04","03","02","01",
+            "12", "11","10","09","08","07","06","05","04","03","02","01")
+kriza2008_meseci <- data.frame(meseci, kriza2008$placa)
+kriza_leto2009 <- kriza2008_meseci[c(1:12), ]
+kriza_leto2008 <- kriza2008_meseci[c(13:24), ]
+kriza_leto2007 <- kriza2008_meseci[c(25:36), ]
 
+ #Kriza leta 2008
+ggplot(kriza_leto2007, mapping=aes(x=meseci, y=kriza2008.placa)) +
+  geom_point(color="red", size=2) +
+  geom_point(kriza_leto2008,mapping=aes(x=meseci, y=kriza2008.placa), color="blue", size=2) +
+  geom_point(kriza_leto2009,mapping=aes(x=meseci, y=kriza2008.placa), color="yellow", size=2) +
+  labs(title="Primerjava plače v letih 2007, 2008, 2009") +
+  ylab("Višina plače(€)") +
+  xlab("Meseci")
+ 
+  #Primerjava leta 2007 z letom 2019
+meseci <- c("09","08","07","06","05","04","03","02","01",
+            "12", "11","10","09","08","07","06","05","04","03","02","01")
+kriza2020_meseci <- data.frame(meseci, kriza2020$placa)
+kriza_leto2019 <- kriza2020_meseci[c(10:21), ]
+
+ggplot(kriza_leto2007, mapping=aes(x=meseci, y=kriza2008.placa)) +
+  geom_point(color="red", size=2) +
+  geom_point(kriza_leto2019, mapping=aes(x=meseci, y=kriza2020.placa), color="blue", size=2) +
+  labs(title="Primerjava plače v letih pred krizo(2007,2019)") +
+  ylab("Višina plače(€)") +
+  xlab("Meseci")
+
+  #Primerjava leta 2008 in 2020(do meseca 09)
+kriza_leto2020 <- kriza2020_meseci[c(1:9), ]
+
+ggplot(kriza_leto2008, mapping=aes(x=meseci, y=kriza2008.placa)) +
+  geom_point(color="red", size=2) +
+  geom_point(kriza_leto2020, mapping=aes(x=meseci, y=kriza2020.placa), color="blue", size=2) +
+  labs(title="Primerjava plače v letih 2008,2020") +
+  ylab("Višina plače(€)") +
+  xlab("Meseci")
