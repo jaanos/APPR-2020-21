26827d916d824e3355fed9dd25657aed25b98bed
Matej Rojec <matej.4001@gmail.com>
2021-01-04 00:25:23 +0100

    Dodal zelo enostaven predikcijski model in poodobil poročilo

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -2,45 +2,44 @@
 
 # Podatke katere bomo modelirali
 
-podatki <- st12 %>% 
-   mutate(emp=parse_number(TOT_EMP, locale=locale(decimal_mark = ",",grouping_mark = ".") ))  %>%
-   mutate(AM=parse_number(A_MEAN, locale=locale(decimal_mark = ".",grouping_mark = ",") )) %>% 
-  mutate(AME=parse_number(A_MEDIAN, locale=locale(decimal_mark = ",",grouping_mark = ".") )) 
-podatki$H_MEAN  <- as.numeric(as.character(podatki$H_MEAN))
-podatki$H_MEDIAN <- as.numeric(as.character(podatki$H_MEDIAN))
-podatki <- podatki  %>% filter(STATE=="Alaska") %>% filter(AM < 33) %>%
-  .[c(1,2,4,6,8,9,10)] %>% drop_na(H_MEAN, H_MEDIAN,AME, AM, emp) %>% rename(HM=H_MEAN, HME=H_MEDIAN)
-imena <- podatki %>% .[c(3,4,5,6,7)] %>% names()
+podatki <- GDP_by_state %>% filter(State=="Alaska")
 
 #Prileganje podatkom
 
-# ggpairs(leto.norm %>% select(H_MEAN, AME, AM, emp))
+# ggpairs(leto.norm %>% select(H_MEAN, leto, leto, GDP))
 
 # Različni modeli predikcij gibanja 
 
-g <- ggplot(podatki, aes(x=AM, y=emp)) + geom_point()
+g <- ggplot(podatki, aes(x=leto, y=GDP)) + geom_point()
 
-lin <- lm(data=podatki, AM ~ emp)
+lin <- lm(data=podatki, leto ~ GDP)
 g1 <- g +  geom_smooth(method="lm", se = FALSE) 
 
-kv <- lm(data=podatki, AM ~ emp + I(emp^2))
-g2 <- ggplot(podatki, aes(x=AM, y=emp)) + 
+kv <- lm(data=podatki, leto ~ GDP + I(GDP^2))
+g2 <- ggplot(podatki, aes(x=leto, y=GDP)) + 
   geom_point() + 
   geom_smooth(method="lm", formula=y ~ x+ I(x^2) ) 
 
 
-mls <- loess(data=podatki, AM ~ emp)
-g3 <- g + geom_smooth(method = "loess") +               
-    scale_x_continuous(name = "AM", breaks = seq(0,30,5)) 
+mls <- loess(data=podatki, leto ~ GDP)
+g3 <- g + geom_smooth(method = "loess")              
 
 
-mgam <- gam(data=podatki, AM ~ s(emp))
-g4 <- g + geom_smooth(method="gam", formula=y ~ s(x), fullrange=TRUE, 
-                      # se=FALSE, 
-                      color="red")
+# modelček
 
-g5 <- g4 +xlim(20,30) + ylim(0,10000)
+quadratic <- lm(data = podatki, GDP ~ I(leto) + I(leto^2))
+years <- data.frame(leto=seq(2019, 2024, 1))
+prediction <- mutate(years, GDP=predict(quadratic, years))
 
-c1 <-cor(podatki$emp,podatki$AM)
+regression <- prediction %>% ggplot(aes(x=leto, y=GDP)) + 
+  geom_smooth(method='lm', fullrange=TRUE, color='red', formula=y ~ poly(x,1,raw=TRUE)) +
+  geom_point(size=2, color="blue") + 
+  scale_x_continuous('Leto', breaks = seq(2019, 2024, 1), limits = c(2019,2024)) +
+  ylab("BDP per capita") +
+  labs(title = "Napoved BDP per capita.")
 
+# primer predikcije
+
+
+# slika 
 
