d039c3fc711a8c3966a721410f8b64ece24b9e64
Ana Marija Okorn <ana-marija.okorn@student.fmf.uni-lj.si>
2021-01-05 10:58:34 +0100

    dodana linearna regresija

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,23 @@
 # 4. faza: Analiza podatkov
 
-#podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-#                                gostota.naselij=naselja/povrsina) %>%
-#  left_join(povprecja, by="obcina")
-#row.names(podatki) <- podatki$obcina
-#podatki$obcina <- NULL
+#LINEARNA REGRESIJA
 
-# Število skupin
-#n <- 5
-#skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+library(ggplot2)
+library(GGally)
+library(mgcv)
+
+g <- ggplot(tabela_tri_zdruzene, aes(x=skupaj.x, y=skupaj.y)) + geom_point()
+#print(g)
+graf9 <- g + geom_smooth(method="lm")
+kv <- lm(data=tabela_tri_zdruzene, skupaj.y ~ skupaj.x + I(skupaj.x^2))
+graf12 <- g + geom_smooth(method="lm", formula=y ~ x + I(x^2)+ I(x^3) + I(x^4) + I(x^5))
+#lin <- lm(data=tabela_tri_zdruzene, skupaj.x ~ skupaj.y)
+#lin
+#predict(lin, data.frame(skupaj.y=seq(100, 800, 100)))
+
+p <- ggplot(tabela_tri_zdruzene, aes(x=skupaj.x, y=skupaj)) + geom_point()
+#print(p)
+graf10 <- p + geom_smooth(method="lm")
+#p + geom_smooth(method="loess")
+kv <- lm(data=tabela_tri_zdruzene, skupaj ~ skupaj.x + I(skupaj.x^2))
+graf11 <- p + geom_smooth(method="lm", formula=y ~ x + I(x^2)+ I(x^3) + I(x^4) + I(x^5))
