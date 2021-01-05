974ea6cbb7f0abe14923d428a6928a2d6edec128
Ana Marija Okorn <ana-marija.okorn@student.fmf.uni-lj.si>
2021-01-05 10:46:09 +0100

    linearna regresija

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -74,3 +74,24 @@ zemljevid2 <- tm_shape(merge(zemljevida, povprecjedipl, by.x="NAME_1", by.y="reg
 #graf4 <- ggplot(z, aes(izobrazevanje, povprecje, group = 1)) + geom_col() + coord_flip()+ labs(x = "Vrsta izobraževanja", y = "Število žensk", 
 #                                                                                               title = "Povprečno število žensk v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih")
 #v graf5 združimo graf3 in graf4
+
+
+#LINEARNA REGRESIJA
+
+library(ggplot2)
+library(GGally)
+library(mgcv)
+
+g <- ggplot(tabela_tri_zdruzene, aes(x=skupaj.x, y=skupaj.y)) + geom_point()
+#print(g)
+graf9 <- g + geom_smooth(method="lm")
+#lin <- lm(data=tabela_tri_zdruzene, skupaj.x ~ skupaj.y)
+#lin
+#predict(lin, data.frame(skupaj.y=seq(100, 800, 100)))
+
+p <- ggplot(tabela_tri_zdruzene, aes(x=skupaj.x, y=skupaj)) + geom_point()
+#print(p)
+graf10 <- p + geom_smooth(method="lm")
+#p + geom_smooth(method="loess")
+kv <- lm(data=tabela_tri_zdruzene, skupaj ~ skupaj.x + I(skupaj.x^2))
+graf11 <- p + geom_smooth(method="lm", formula=y ~ x + I(x^2)+ I(x^3) + I(x^4) + I(x^5))
