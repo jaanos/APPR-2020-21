dc654c750c6db1f514defac68d519cd5142e20fa
Ana Marija Okorn <ana-marija.okorn@student.fmf.uni-lj.si>
2021-01-05 09:01:06 +0100

    popravek legend

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -20,29 +20,35 @@ graf1 <- ggplot(povprecje.dijakov.po.regijah, aes(regija, povprecje, group = 1))
 
 zemljevid <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", "gadm36_SVN_1")
 zemljevid1 <- tm_shape(merge(zemljevid, povprecje.diplomantov.po.regijah, by.x="NAME_1", by.y="regija" )) + 
-  tm_polygons("povprecje",title="Povprečje") + tm_layout(title="Povprečno število diplomantov po regijah v zadnjih desetih letih")
+  tm_polygons("povprecje",title="Povprečje",palette="Purples") + tm_style("grey") + tm_layout(title="Povprečno število diplomantov po regijah v zadnjih desetih letih")
 
 # GRAF POVPRECJA DIJAKOV GLEDE NA VRSTO IZOBRAZEVANJA IN SPLOL
 
 podatki5 <- tabela2nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10)
 graf5 <- ggplot(podatki5, aes(x=izobrazevanje, y=povprecje, fill = spol)) + geom_col(position = 'dodge')  + 
-  coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "Povprečno število moških in žensk na leto v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih")
+  coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "Povprečno število moških in žensk na leto v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih") +
+  scale_fill_discrete(name = "Spol", labels = c("moški", "ženski"))
 
 # GRAF POVPRECJA DIPLOMANTOV GLEDE NA VRSTO IZOBRAZEVANJA IN SPLOL
 
 podatki6 <- tabela3nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10)
 graf6 <- ggplot(podatki6, aes(x=izobrazevanje, y=povprecje, fill = spol)) + geom_col(position = 'dodge')  + 
-  coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "Povprečno število diplomantov na leto  \nposameznih vrstah izobraževanj v \nzadnjih desetih letih")
+  coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "Povprečno število diplomantov na leto  \nposameznih vrstah izobraževanj v \nzadnjih desetih letih") +
+  scale_fill_discrete(name = "Spol", labels = c("moški", "ženski"))
 
 # TORTNI DIAGRAM DIPLOMANTOV GLEDE NA NACIN STUDIJA
 
 f <- tabela4nova %>% group_by(studij) %>% summarise(vsota=sum(stevilo))
-tortnidiagram <- ggplot(f) + aes(x="", y = vsota,fill=studij) + geom_col(width=1) + coord_polar(theta="y") + xlab("") + ylab("")
+tortnidiagram <- ggplot(f) + aes(x="", y = vsota,fill=studij) + geom_col(width=1) + coord_polar(theta="y") + xlab("") + ylab("") + scale_fill_discrete(name = "Študij", labels = c("izredni", "rednii"))
 tortni <-  ggplot(tabela4nova %>% group_by(studij) %>% summarise(delez=100 * n() / nrow(tabela4nova))) +
   aes(x="", y=delez, fill=studij) + geom_col(width=1) +
   coord_polar(theta="y") + xlab("") + ylab("") +
   scale_y_continuous(breaks=seq(0, 100, 10),
                      labels=paste(seq(0, 100, 10), "%"))
+tortni2 <- ggplot(tabela4nova %>% group_by(studij) %>% summarise(delez=100 * n() / nrow(tabela4nova)),
+                  aes(x="", y=delez, fill = studij)) +
+  geom_bar(width = 1, stat="identity", colour="black") + coord_polar("y", start=0)+
+  xlab("") + ylab("") + theme(axis.text.x=element_blank(), panel.grid=element_blank())
 
 # GRAF SPREMINJANJA STEVILA TUJCEV IN LJUDI, KI IMAJO NEZNANO BIVALISCE, MED DIJAKI IN DIPLOMANTI PO LETIH
 
@@ -56,7 +62,7 @@ graf8 <- povprecjedipl %>% ggplot(aes(x=povprecje, y=regija, fill = regija)) + g
 
 zemljevida <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", "gadm36_SVN_1")
 zemljevid2 <- tm_shape(merge(zemljevida, povprecjedipl, by.x="NAME_1", by.y="regija" )) + 
-  tm_polygons("povprecje",title="Delež") + tm_layout(title="Povprečni delež diplomantov po regijah v zadnjih desetih letih glede na število prebivalcev v posamezni regiji")
+  tm_polygons("povprecje",title="Delež",palette="Purples")+ tm_style("grey") + tm_layout(title="Povprečni delež diplomantov po regijah v zadnjih desetih letih glede na število prebivalcev v posamezni regiji")
 
 #graf2 <- ggplot(povprecje.diplomantov.po.regijah, aes(regija, povprecje, group = 1)) + geom_col() + coord_flip() +
 #  labs(x = "Regija", y = "Povprecje", 
