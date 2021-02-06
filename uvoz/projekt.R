06c77d1ef78c080f9aa473fdb42b558c1944ab2d
David Rozman <david.rozman8@gmail.com>
2021-02-06 17:37:29 +0100

    Premešal mape, dodal novo tabelo

--- uvoz/projekt.R
+++ uvoz/projekt.R
@@ -5,15 +5,20 @@ require(readr)
 require(dplyr)
 
 leta <- c(7, 8, 9, 15, 16, 17, 18, 19, 20)
-drzave <- c("SLO"="Slovenia", "ZDA"="United States of America")
+drzave <- c("SLO"="Slovenija", "ZDA"="United States of America")
 podatki <- lapply(leta, function(leto)
   lapply(names(drzave), function(drzava)
-    sprintf('projekt/%s%02d.xlsx', drzava, leto) %>% read_xlsx() %>%
+    sprintf('podatki/%s%02d.xlsx', drzava, leto) %>% read_xlsx() %>%
       mutate(Drzava=drzave[drzava], Leto=2000+leto)) %>% bind_rows()) %>%
   bind_rows() %>% transmute(Leto, Drzava, Mesto, Film,
                             Zasluzek=parse_number(`Zaslužek v $`))
 
-podatki %>% drop_na(Mesto)
+
+podatki %>% drop_na(Zasluzek)
+
+zasluzki.po.letih <- data.frame(leta+2000)
+names(zasluzki.po.letih)[1] <- 'Leto'
+
 
 
 require(rvest)
