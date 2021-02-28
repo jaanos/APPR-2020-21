9342235725180ee6d9dd6b5f7431bc6827578bba
unknown <ian.spiller11@gmail.com>
2021-02-28 13:14:14 +0100

    Uvoz koda

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,55 +1,178 @@
-# 2. faza: Uvoz podatkov
+#UVOZ IN OBDELAVA PODATKOV
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.character(tabela[[col]])) {
-      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-    }
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
+#UVOZ IZ SEC
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
-                    locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- iconv("Sveti Jurij ob Ščavnici", to="UTF-8")
-  data <- data %>% pivot_longer(`1`:`4`, names_to="velikost.druzine", values_to="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- parse_factor(data$obcina, levels=obcine)
-  return(data)
-}
+link <- "https://www.sec.gov/Archives/edgar/data/320193/000032019319000119/a10-k20199282019.htm#sDBCC0D7FC5D05F49A572F9AA0627E992"
+stran <- html_session(link) %>% read_html()
+podatki_prodaja_svet <- stran %>% html_nodes(xpath="//*[text() = 'Americas:']/ancestor::table[1]") %>%
+  .[[1]] %>% html_table() %>% 
+  transmute("Podatki"=parse_character(X1, na=""),"2019"=X3,"2018"=X7,"2017"=X11) %>%
+  drop_na(Podatki) %>% mutate(Regija=ifelse(`2018` == "", str_replace(Podatki, ":", ""), NA)) %>%
+  fill(Regija) %>% pivot_longer(c(-Regija, -Podatki), names_to="Leto", values_to="Vrednost") %>%
+mutate(Leto=parse_number(Leto), Vrednost=parse_number(Vrednost)) %>% drop_na(Vrednost)
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
+#UVOZ IZ MORNINGSTARJA
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+podatki <- read_csv("podatki/AAPL.csv",locale = locale(encoding = "UTF-8"),skip = 2) %>%
+  slice(1:16) %>% rename("Podatki"=X1)
+podatki<-podatki[,-12]
+morningstar <- as.data.frame(t(podatki[-1])) %>% select("V1","V5","V6","V7","V10") %>%
+  rownames_to_column(var="A") %>% mutate(A=str_sub(A,1,4))
+colnames(morningstar) <- c("Leto","Prodaja","Neto_dobicek",
+                           "Neto_dobicek_na_delnico",
+                           "Dividenda","Knjigovodska_vrednost")
+j<- 1:6
+morningstar[,j] <- lapply(morningstar[,j],function(x) {as.numeric(gsub(",","", x))})
+
+
+#UVOZ IZ YAHOO
+
+yahoo<-as.data.frame(getSymbols("AAPL",from = "2011-01-01", to = "2021-01-01",
+                                periodicity = "monthly", auto.assign = FALSE)) %>%
+  rownames_to_column(var="Leta")%>%filter(grepl("-12-01",Leta))%>%
+  rename("Najvisja_cena"="AAPL.High")%>%mutate(Leto=str_sub(Leta,1,4))%>%
+  select("Leto","Najvisja_cena")
+yahoo[1]<-lapply(yahoo[1],function(x) as.numeric(x))
+
+
+#UVOZ QUANDL 
+
+podatki_quandl_pe<-Quandl("MULTPL/SP500_PE_RATIO_YEAR",collapse="annual",
+                          api_key="mqxb_zyDyu5wbiG5qtTx",
+                          start_date="2010-08-01",
+                          end_date="2020-8-31") %>%
+  rename("Datum"=Date,"P.E_SP500"=Value) %>% 
+  mutate(Leto=as.integer(format(Datum,"%Y"))) %>%
+  select("Leto","P.E_SP500" )
+podatki_quandl_pe <- podatki_quandl_pe[order(podatki_quandl_pe$Leto),]
+
+
+podatki_quandl_pb<-Quandl("MULTPL/SP500_PBV_RATIO_YEAR",collapse="annual",
+                          api_key="mqxb_zyDyu5wbiG5qtTx",
+                          start_date="2011-08-01",
+                          end_date="2020-8-31") %>%
+  rename("Datum"=Date,"P.B_SP500"=Value) %>%
+  mutate(Leto=as.integer(format(Datum,"%Y"))) %>%
+  select("Leto","P.B_SP500" )
+podatki_quandl_pb <- podatki_quandl_pb[order(podatki_quandl_pb$Leto),]
+
+
+podatki_quandl_prodaja<-Quandl("MULTPL/SP500_SALES_YEAR",collapse="annual",
+                               api_key="mqxb_zyDyu5wbiG5qtTx",
+                               start_date="2011-08-01",
+                               end_date="2020-8-31") %>%
+  rename("Datum"=Date,"Prodaja_SP500"=Value) %>% 
+  mutate(Leto=as.integer(format(Datum,"%Y"))) %>% 
+  select("Leto","Prodaja_SP500" )
+podatki_quandl_prodaja <- podatki_quandl_prodaja[order(podatki_quandl_prodaja$Leto),]  
+
+
+podatki_quandl_earning<-Quandl("MULTPL/SP500_EARNINGS_YEAR",collapse="annual",
+                               api_key="mqxb_zyDyu5wbiG5qtTx",
+                               start_date="2011-08-01",
+                               end_date="2020-8-31") %>%
+  rename("Datum"=Date,"Earning_SP500"=Value) %>%
+  mutate(Leto=as.integer(format(Datum,"%Y"))) %>%
+  select("Leto","Earning_SP500" )
+podatki_quandl_earning <- podatki_quandl_earning[order(podatki_quandl_earning$Leto),] 
+ 
+
+podatki_quandl_bv<-Quandl("MULTPL/SP500_BVPS_YEAR",collapse="annual",
+                          api_key="mqxb_zyDyu5wbiG5qtTx",
+                          start_date="2011-08-01",
+                          end_date="2020-8-31") %>%
+  rename("Datum"=Date,"BV_SP500"=Value) %>%
+  mutate(Leto=as.integer(format(Datum,"%Y"))) %>%
+  select("Leto","BV_SP500" )
+podatki_quandl_bv <- podatki_quandl_bv[order(podatki_quandl_bv$Leto),]  
+
+
+podatki_quandl_dividenda <- Quandl("MULTPL/SP500_DIV_YEAR",collapse="annual",
+                                   api_key="mqxb_zyDyu5wbiG5qtTx",
+                                   start_date="2011-08-01",
+                                   end_date="2020-8-31")%>%
+  rename("Datum"=Date,"Dividenda_SP500"=Value) %>% 
+  mutate(Leto=as.integer(format(Datum,"%Y"))) %>%
+  select("Leto","Dividenda_SP500" )
+podatki_quandl_dividenda <- podatki_quandl_dividenda[order(podatki_quandl_dividenda$Leto),]
+
+#KONTINENTI
+
+kontinenti <- read_csv("podatki/kontinenti1.csv",locale = locale(encoding = "UTF-8")) %>%
+  select("Continent_Name","Country_Name","Three_Letter_Country_Code") %>%
+  rename("GU_A3"="Three_Letter_Country_Code")
+
+
+#IZRACUNI PE PB
+
+PE <- select(morningstar,"Leto","Neto_dobicek_na_delnico") %>% left_join(yahoo)%>%
+  mutate(PE=Najvisja_cena/Neto_dobicek_na_delnico) %>% select("Leto","PE")
+  
+PB <- select(morningstar,"Leto","Knjigovodska_vrednost") %>% left_join(yahoo)%>%
+  mutate(PB=Najvisja_cena/Knjigovodska_vrednost)%>%
+  select("Leto","PB")
+  
+
+#FUNKCIJA ZA RAST
+
+rast <- function(tabela,b){
+  a <- mutate(tabela,Rast=((tabela[[b]]-lag(tabela[[b]]))/lag(tabela[[b]]))*100)%>%
+    select("Leto","Rast")
+  return(a)
+} 
+
+
+#IZRACUNANE RASTI
+
+Rast_prodaje <- rast(morningstar,"Prodaja")
+  
+Rast_dobicka <-  rast(morningstar,"Neto_dobicek")
+  
+Rast_knjigovodske_vrednosti <-  rast(morningstar,"Knjigovodska_vrednost")
+  
+Rast_dividende <- rast(morningstar,"Dividenda")
+
+Rast_SP_prodaja <- rast(podatki_quandl_prodaja,"Prodaja_SP500")
+
+Rast_SP_dobicka <- rast(podatki_quandl_earning,"Earning_SP500")
+  
+Rast_SP_knjigovodske_vrednosti <- rast(podatki_quandl_bv,"BV_SP500")
+
+Rast_SP_dividende <- rast(podatki_quandl_dividenda,"Dividenda_SP500")
+
+
+#ZAPIS PODATKOV V CSV OBLIKO
+#write.csv(podatki_prodaja_svet,
+#          "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_SEC.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(morningstar,
+#          "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_morningstar.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(yahoo,
+#          "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_yahoo.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(podatki_quandl_pe,
+#          "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_quandl_pe.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(podatki_quandl_pb,
+#         "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_quandl_pb.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(podatki_quandl_prodaja,
+#          "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_quandl_prodaja.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(podatki_quandl_earning,
+#          "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_quandl_dobicek.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(podatki_quandl_dividenda,
+#          "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_quandl_dividenda.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(podatki_quandl_bv,
+#          "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_quandl_bv.csv",
+#          fileEncoding = "UTF-8",row.names=FALSE)
+#write.csv(kontinenti,
+#         "C:\\Users\\PC\\Documents\\Sola\\2. letnik\\Obdelava podatkov R\\Projekt\\APPR-2020-21\\podatki\\Precisceni_kontinenti.csv",
+#         fileEncoding = "UTF-8",row.names=FALSE)
+
+
