defaf8b7297b962b5008a03717ca75340c53bda5
jankoprogramko <31898545+jankoprogramko@users.noreply.github.com>
2021-07-21 15:48:59 +0200

    odstranjen stolpec name iz stat tabel

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -5,14 +5,14 @@ library("rlang")
 uvoz17 <- read_csv("podatki/merged_gw_17.csv",
                       TRUE, locale=locale(encoding="Windows-1252"),
                       col_types=cols("was_home"=col_logical()))
-stat17 <- uvoz17[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:56)]
+stat17 <- uvoz17[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:55)]
 uvoz18 <- read_csv("podatki/merged_gw_18.csv", TRUE, locale=locale(encoding="Windows-1252"),
                       col_types=cols("was_home"=col_logical()))
-stat18 <- uvoz18[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:56)]
+stat18 <- uvoz18[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:55)]
 
 uvoz19 <- read_csv("podatki/merged_gw_19.csv", TRUE, locale=locale(encoding="Windows-1252"),
                       col_types=cols("was_home"=col_logical()))
-stat19 <- uvoz19[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:56)]
+stat19 <- uvoz19[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:55)]
 
 uvoz_igralci17 <- read_csv("podatki/players_raw_17.csv", TRUE,
                       locale=locale(encoding="UTF-8"))
