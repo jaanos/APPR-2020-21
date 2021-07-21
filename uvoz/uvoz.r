895f82e7448c1dc4a0deb849e3c81a724302a444
jankoprogramko <31898545+jankoprogramko@users.noreply.github.com>
2021-07-21 14:55:37 +0200

    odstranil nepotrebne podatke

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,55 +1,28 @@
-# 2. faza: Uvoz podatkov
+library("readr")
+library("tidyr")
+library("rlang")
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+uvoz17 <- read_csv("podatki/merged_gw_17.csv",
+                      TRUE, locale=locale(encoding="Windows-1252"),
+                      col_types=cols("was_home"=col_logical()))
+stat17 <- uvoz17[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:56)]
+uvoz18 <- read_csv("podatki/merged_gw_18.csv", TRUE, locale=locale(encoding="Windows-1252"),
+                      col_types=cols("was_home"=col_logical()))
+stat18 <- uvoz18[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:56)]
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.character(tabela[[col]])) {
-      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-    }
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
+uvoz19 <- read_csv("podatki/merged_gw_19.csv", TRUE, locale=locale(encoding="Windows-1252"),
+                      col_types=cols("was_home"=col_logical()))
+stat19 <- uvoz19[c(2:5,8:10,12,15:20,22,24,29:31,33:40,42:44,53:56)]
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
-                    locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- iconv("Sveti Jurij ob Ščavnici", to="UTF-8")
-  data <- data %>% pivot_longer(`1`:`4`, names_to="velikost.druzine", values_to="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- parse_factor(data$obcina, levels=obcine)
-  return(data)
-}
+uvoz_igralci17 <- read_csv("podatki/players_raw_17.csv", TRUE,
+                      locale=locale(encoding="UTF-8"))
+info17 <- uvoz_igralci17[c(15,19,24,41,46:47,56)]
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
+uvoz_igralci18 <- read_csv("podatki/players_raw_18.csv", TRUE,
+                      locale=locale(encoding="UTF-8"))
+info18 <- uvoz_igralci18[c(15,19,24,42,47:48,57)]
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
+uvoz_igralci19 <- read_csv("podatki/players_raw_19.csv", TRUE,
+                      locale=locale(encoding="UTF-8"))
+info19 <- uvoz_igralci19[c(15,19,24,42,47:48,57)]
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
