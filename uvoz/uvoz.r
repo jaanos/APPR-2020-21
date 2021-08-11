a247cf3e1fd96f472570a27e8a26b668852f2d30
jankoprogramko <31898545+jankoprogramko@users.noreply.github.com>
2021-08-11 15:24:17 +0200

    metodao merge zamenjal z left_join

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -15,12 +15,12 @@ stat17 <- uvoz17[c(1:5,8:10,12,15:16,18:20,24,29:40,42:44,53:55)] %>%
 uvoz18 <- read_csv("podatki/merged_gw_18.csv", TRUE, locale=locale(encoding="Windows-1252"),
                       col_types=cols("was_home"=col_logical()))
 stat18 <- uvoz18[c(1:5,8:10,12,15:16,18:20,24,29:40,42:44,53:55)] %>%
-  separate(name, c("first_name", "second_name"), sep="_") #
+  separate(name, c("first_name", "second_name"), sep="_")
 
 uvoz19 <- read_csv("podatki/merged_gw_19.csv", TRUE, locale=locale(encoding="Windows-1252"),
                       col_types=cols("was_home"=col_logical()))
 stat19 <- uvoz19[c(1:5,8:10,12,15:16,18:20,24,29:40,42:44,53:55)] %>%
-  separate(name, c("first_name", "second_name", "id"), sep = "_") %>% select(-id)
+  separate(name, c("first_name", "second_name", "id"), sep = "_") %>% mutate(id = parse_number(id))
 
 uvoz_igralci17 <- read_csv("podatki/players_raw_17.csv", TRUE,
                       locale=locale(encoding="UTF-8"))
@@ -42,13 +42,16 @@ uvoz_igralci19 <- read_csv("podatki/players_raw_19.csv", TRUE,
 #kres17 <- filter(stat17, second_name=="Cresswell")
 #kres18 <- filter(stat18, second_name=="Cresswell")
 #kres19 <- filter(stat19, second_name=="Cresswell")
-
+skupna17 <- stat17 %>% left_join(uvoz_igralci17[c(15,19,24,41,46,56)])
+skupna18 <- stat18 %>% left_join(uvoz_igralci18[c(15,19,24,42,47,57)])
+#dvakrat danny ward
+skupna19 <- stat19 %>% left_join(uvoz_igralci19[c(15,19,24,42,47,57)])
 #združitev tabele s statistikami s tabelo z informacijami
-skupna17 <- merge(stat17, uvoz_igralci17[c(15,19,24,41,46,56)]) #%>% select(-fixture, -team_code)
-skupna18 <- merge(stat18, uvoz_igralci18[c(15,19,24,42,47,57)]) #%>% select(-fixture, -team_code)
-skupna19 <- merge(stat19, uvoz_igralci19[c(15,19,24,42,47,57)]) #%>% select(-fixture, -team_code)
+#skupna17 <- merge(stat17, uvoz_igralci17[c(15,19,24,41,46,56)]) #%>% select(-fixture, -team_code)
+#skupna18 <- merge(stat18, uvoz_igralci18[c(15,19,24,42,47,57)]) #%>% select(-fixture, -team_code)
+#skupna19 <- merge(stat19, uvoz_igralci19[c(15,19,24,42,47,57)]) #%>% select(-fixture, -team_code)
 
-#komulativne tabele za igralce, odstranili smo stolpec, ki belezi nasprotnika v krogu, krog, domace igrisce 
+#komulativne tabele za igralce, odstranili smo stolpec, ki beleži nasprotnika v krogu, krog, domace igrisce 
 kom17 <- skupna17[c(-20,-27,-32)] %>%
   group_by(first_name, second_name, element_type, id, team, web_name) %>%
   summarise(across(everything(), sum))
