7e34e9516ae913d571c3ae30e36db6ed7c468ab3
jankoprogramko <31898545+jankoprogramko@users.noreply.github.com>
2021-08-11 14:00:01 +0200

    v libraries dodal nove knjižnice in možnost klicanja v uvoz

--- lib/libraries.r
+++ lib/libraries.r
@@ -4,8 +4,14 @@ library(gsubfn)
 library(tidyr)
 library(tmap)
 library(shiny)
+library("readr")
+#library("tidyr")
+library("rlang")
+library("dplyr")
+library(ggplot2)
+
 
 options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
-source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+#source("lib/uvozi.zemljevid.r", encoding="UTF-8")
