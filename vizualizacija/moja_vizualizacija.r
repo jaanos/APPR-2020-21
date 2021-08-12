8c79d0a576ff2af506a852755f648254ab6c3fe6
jankoprogramko <31898545+jankoprogramko@users.noreply.github.com>
2021-08-12 14:44:05 +0200

    nadaljevanje vizualizacije

--- vizualizacija/moja_vizualizacija.r
+++ vizualizacija/moja_vizualizacija.r
@@ -12,7 +12,7 @@ goli_po_krogih19 <- skupna19[c("round", "goals_scored", "own_goals")] %>% group_
 
 razp <-cbind(goli_po_krogih17,goli_po_krogih18, goli_po_krogih19)
 
-
+f <-function(x) {if (x == TRUE) {x="Doma"} else {x="V gosteh"}}
 ggplot(cbind(skupna17[c("round", "goals_scored", "own_goals")] %>% group_by(round) %>%
                summarise(st_zadetkov_17 = sum(goals_scored + own_goals)),
              skupna18[c("round", "goals_scored", "own_goals")] %>% group_by(round) %>%
@@ -33,4 +33,27 @@ ggplot(cbind(skupna17[c("round", "goals_scored", "own_goals")] %>% group_by(roun
 #               summarise(st_zadetkov = sum(goals_scored + own_goals))) + 
 #  aes(x=round, y=st_zadetkov) + geom_line()
 
+goli_po_sezonah <- cbind(skupna17[c("goals_scored", "own_goals","was_home")] %>% group_by(was_home) %>%
+  summarise("2016/17"=sum(goals_scored + own_goals)) %>% rename(Teren=was_home),
+  skupna18[c("goals_scored", "own_goals","was_home")] %>% group_by(was_home) %>%
+    summarise("2017/18"=sum(goals_scored + own_goals)) %>% select(-was_home),
+  skupna19[c("goals_scored", "own_goals","was_home")] %>% group_by(was_home) %>%
+    summarise("2018/19"=sum(goals_scored + own_goals)) %>% select(-was_home)) %>%
+  pivot_longer(-Teren,names_to="sezona", values_to="stevilo_golov") %>%
+  mutate(Teren=sapply(Teren, function(x) {if (x == TRUE) {x="Doma"} else {x="V gosteh"}}))
 
+#podatki <- goli_po_sezonah %>% pivot_longer(-Teren,names_to="sezona", values_to="št. golov")
+ggplot(goli_po_sezonah) + aes(x=sezona, y=stevilo_golov, fill=Teren) + geom_col(position="dodge") +
+  geom_text(aes(label = stevilo_golov), vjust=1.5, position=position_dodge(0.9)) +
+  ggtitle("Število zadetkov doma in v gosteh") + ylab("Število zadetkov") + xlab("Sezona")
+
+goli_pozicija <- kom17[c("goals_scored","element_type")] %>% group_by(element_type) %>%
+  summarise(odstotek_zadetkov = sum(goals_scored)/sum(kom17$goals_scored) * 100) %>%
+  mutate(element_type = c("Vratar", "Branilec", "Vezist", "Napadalec")[element_type]) %>%
+  rename(Pozicija=element_type) %>% filter(odstotek_zadetkov > 0) %>%
+  mutate(odstotek_zadetkov=round(odstotek_zadetkov,2))
+
+ggplot(goli_pozicija) + aes(x="", y=odstotek_zadetkov, fill=Pozicija) + geom_col() +
+  coord_polar(theta="y") + geom_text(aes(label=odstotek_zadetkov))
+pie(goli_pozicija$odstotek_zadetkov, labels=paste(goli_pozicija$Pozicija, goli_pozicija$odstotek_zadetkov, "%"),
+    main="Odstotek zadetkov po igralnih pozicijah")
