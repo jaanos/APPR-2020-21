7b8749234be80f2a027873a830b4f4bcee94b682
klarasirca <klara.sirca@gmail.com>
2021-08-25 20:29:08 +0200

    Uvozila še par tabelic

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -78,8 +78,35 @@ tabela5$Drzava_prihodnjega_bivalisca[tabela5$Drzava_prihodnjega_bivalisca == "JU
 tabela5$Drzava_prihodnjega_bivalisca[tabela5$Drzava_prihodnjega_bivalisca == "AVSTRALIJA IN OCEANIJA"] <- "Avstralija in Oceanija"
 tabela5$Drzava_prihodnjega_bivalisca[tabela5$Drzava_prihodnjega_bivalisca == "SEVERNA IN SREDNJA AMERIKA"] <- "Severna in Srednja Amerika"
 
+#________________________________ TABELA 6 ___________________________________________
+imenastolpcev6 <- c("Leto", "Drzava", "Drzavljanstvo", "Spol", "Izobrazba", "Stevilo")
+tabela6 <- read_csv2("podatki/priseljeni_izobrazba.csv", na=c("..."), col_names = imenastolpcev6, skip=3,
+                     locale=locale(encoding = "Windows-1250"))
 
+tabela6$Drzavljanstvo <- NULL
+tabela6$Drzava[tabela6$Drzava == "AFRIKA"] <- "Afrika"
+tabela6$Drzava[tabela6$Drzava == "AZIJA"] <- "Azija"
+tabela6$Drzava[tabela6$Drzava == "JUŽNA AMERIKA"] <- "Južna Amerika"
+tabela6$Drzava[tabela6$Drzava == "AVSTRALIJA IN OCEANIJA"] <- "Avstralija in Oceanija"
+tabela6$Drzava[tabela6$Drzava == "SEVERNA IN SREDNJA AMERIKA"] <- "Severna in Srednja Amerika"
+tabela6 <- subset(tabela6, Drzava!="Država prejšnjega prebivališča - SKUPAJ")
+tabela6 <- subset(tabela6, Drzava!="EVROPA - SKUPAJ")
+tabela6 <- subset(tabela6, Drzava!="- Države nastale na območju nekdanje Jugoslavije - SKUPAJ")
+tabela6 <- subset(tabela6, Drzava!="Države EU - SKUPAJ")
+tabela6 <- subset(tabela6, Drzava!="- Druge evropske države")
+tabela6$Drzava <- gsub("\\.\\.\\.\\.","",tabela6$Drzava)
 
+#___________________ TABELA 7 ______________________________________________
 
+imenastolpcev7 <- c("Leto", "Drzava", "Namen", "Stevilo")
+tabela7 <- read_csv2("podatki/priseljeninamen.csv", na=c("..."), col_names = imenastolpcev7, skip=2,
+                     locale=locale(encoding = "Windows-1250"))
+tabela7$Drzava <- gsub("\\.\\.\\.\\.","", tabela7$Drzava)
+tabela7 <- tabela7 %>% filter(between(Leto,2011,2021))
+
+tabela7 <- subset(tabela7, Drzava!="Neznano")
+tabela7 <- subset(tabela7, Drzava!="Srbija in Črna gora")
+tabela7 <- subset(tabela7, Drzava!="Države EU")
+tabela7$Drzava[tabela7$Drzava == "... druge države članice EU"] <- "ostale države članice EU"
 
 
