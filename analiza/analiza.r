576caf5e38864287c55bd66c3f599c9aaf49e76f
David Rozman <david.rozman8@gmail.com>
2021-08-31 12:55:54 +0200

    Projekt, poslan po e-pošti

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,22 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
+tabelazaanalizo <- tabela2[,c(1,2)]
+tabelazaanalizo$LETO <- as.numeric(as.character(tabelazaanalizo$LETO))
+samopike <- ggplot(tabelazaanalizo, aes(x=LETO, y=SLOVENIJA)) + geom_point()
+print(samopike)
+model <- lm(SLOVENIJA ~ LETO, data=tabelazaanalizo)
+napoved <- ggplot(tabelazaanalizo, aes(x=LETO, y=SLOVENIJA)) + geom_point() +
+          geom_smooth(method=lm, formula=y~x) +
+          ylab("Dobavljena voda na prebivalca v m^3")
+print(napoved)
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+prihodnjaleta <- data.frame(LETO=c(2020:2025))
+predict(model, prihodnjaleta)
+napoved1 <- prihodnjaleta %>% mutate(SLOVENIJA=predict(model, prihodnjaleta))
+napoved1$SLOVENIJA <- round(napoved1$SLOVENIJA, digit=1)
+napoved1 <- rbind(tabelazaanalizo, napoved1)
+izrisnapovedi <- ggplot(napoved1, aes(x=LETO, y=SLOVENIJA)) +
+  geom_point(shape=1) +
+  geom_smooth(method=lm, formula=y~x) 
+#  geom_point(data=napoved1, aes(x=LETO, y=dobavljenavoda), color="red", size=2)
+print(izrisnapovedi)
---
+++ lib/libraries.r
@@ -0,0 +1,22 @@
+library(knitr)
+library(rvest)
+library(gsubfn)
+library(tidyr)
+library(tmap)
+library(shiny)
+library(readr)
+library(dplyr)
+library(readxl)
+library(openxlsx)
+require(ggplot2)
+require(rvest)
+require(xml2)
+require(maptools)
+require(rgdal)
+require(rgeos)
+require(janitor)
+
+options(gsubfn.engine="R")
+
+# Uvozimo funkcije za pobiranje in uvoz zemljevida.
+source("lib/uvozi.zemljevid.r", encoding="UTF-8")
